55561140f7441730797bd7a0eca47ed5309a97ec
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2018-09-04 10:56:41 -0400

    Kill db, implement web and API (#1)
    * Working web init/update/teardown
    
    * Return status from web
    
    * Working API component
    
    * Working web init/update/teardown
    
    * Return status from web
    
    * Working API component
    
    * Add documentation

--- fup/__init__.py
+++ fup/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from typing import List
+import json
 import sys
 
 import boto3
@@ -53,9 +54,9 @@ class StackModel(Model):
 
     stack_name = UnicodeAttribute(hash_key=True)
     schema = MapAttribute(null=True)
-    db_deployed = BooleanAttribute(default=False)
-    api_deployed = BooleanAttribute(default=False)
-    web_deployed = BooleanAttribute(default=False)
+    db_deployed = UnicodeAttribute(default="")
+    api_deployed = UnicodeAttribute(default="")
+    web_deployed = UnicodeAttribute(default="")
 
 
 class fupclient:
@@ -68,7 +69,6 @@ class fupclient:
             region_name=self.aws_region,
             profile_name=self.aws_profile
         )
-        # self.session.resource('dynamodb')
         self.stack_db = StackModel
 
     def _log(self, msg: str, level=0):
@@ -105,37 +105,8 @@ class fupclient:
             return True
         return False
 
-    def teardown(self, stack_name: str) -> bool:
-        self._log(stylize(
-            f"Tearing down [{stack_name}]...",
-            colored.fg("red"), colored.attr('bold')
-        ))
-        # TODO: Tear down deployed assets
-        stack = self.stack_db.get(stack_name)
-        if stack.db_deployed:
-            # TODO: Tear down deployed assets
-            pass
-        self._success(f"Successfully tore down stack [{stack_name}].")
-        return stack.delete()
-
-    def get_stacks(self, pending=None):
-        try:
-            if pending is None:
-                response = [
-                    i.stack_name for i in self.stack_db.scan()
-                ]
-            else:
-                response = [
-                    i.stack_name for i in self.stack_db.scan(
-                        self.stack_db.api_deployed != pending
-                    )
-                ]
-            return response
-        except:
-            return []
-
     def init(
-        self, stack_name: str, component_list=[FupComponents.ALL],
+        self, stack_name: str, component_list=FupComponents.ALL,
         web_path: str = "./web",
         api_path: str = "./api",
         db_path: str = "./schema.yaml"
@@ -159,13 +130,159 @@ class fupclient:
         except:
             pass
 
-        if FupComponents.DB in component_list:
-            self._info(f"Creating component [{stack_name}.DB]...")
-            components.DBComponent(schemafile=db_path).init()
+        db_deployed = "0"
+        api_deployed = "0"
+        web_deployed = "0"
+        if FupComponents.WEB in component_list:
+            self._info(f"Creating component [{stack_name}.WEB]...")
+            web_deployed = components.WebComponent(
+                stack_name=stack_name,
+                directory=web_path
+            ).init()
+        if FupComponents.API in component_list:
+            self._info(f"Creating component [{stack_name}.API]...")
+            api_deployed = components.APIComponent(
+                stack_name=stack_name,
+                directory=api_path
+            ).init()
+        # if FupComponents.DB in component_list:
+        #     self._info(f"Creating component [{stack_name}.DB]...")
+        #     components.DBComponent(schemafile=db_path).init()
 
         new_stack = StackModel(stack_name)
+        new_stack.web_deployed = web_deployed
+        new_stack.api_deployed = api_deployed
+        new_stack.db_deployed = db_deployed
+        self._info("Uploading stack configuration...")
         new_stack.save()
         self._log(stylize(
             "Created stack [{}].".format(stack_name),
             colored.fg("green")
         ))
+
+    def update(
+        self, stack_name: str, component_list=FupComponents.ALL,
+        web_path: str = "./web",
+        api_path: str = "./api",
+        db_path: str = "./schema.yaml"
+    ) -> None:
+        # Fail if stack DNE:
+        if not self.stack_db.exists():
+            self._warn("Stack lookup table does not exist, try `init`.")
+            sys.exit(1)
+
+        stack = self.stack_db.get(stack_name)
+
+        if FupComponents.WEB in component_list:
+            self._info(f"Updating component [{stack_name}.WEB]...")
+            stack.web_deployed = components.WebComponent(
+                stack_name=stack_name,
+                directory=web_path
+            ).update()
+
+        if FupComponents.API in component_list:
+            self._info(f"Updating component [{stack_name}.API]...")
+            if (
+                    (stack.api_deployed == "0") or  # placeholder falsy
+                    (not stack.api_deployed) or     # actual falsy
+                    (stack.api_deployed == "null")  # dynamo falsky
+            ):
+                stack.api_deployed = components.APIComponent(
+                    stack_name=stack_name,
+                    directory=api_path
+                ).update()
+            else:
+                stack.api_deployed = components.APIComponent(
+                    stack_name=stack_name,
+                    directory=api_path,
+                    config=json.loads(stack.api_deployed)
+                ).update()
+
+        self._info("Uploading stack configuration...")
+        stack.save()
+        self._success(
+            "Updated stack [{}].".format(stack_name)
+        )
+
+
+    def status(
+        self, stack_name: str, component_list=FupComponents.ALL,
+        web_path: str = "./web",
+        api_path: str = "./api",
+        db_path: str = "./schema.yaml"
+    ) -> dict:
+        # Fail if stack DNE:
+        if not self.stack_db.exists():
+            self._warn("Stack lookup table does not exist, try `init`.")
+            sys.exit(1)
+
+        stack = self.stack_db.get(stack_name)
+        outputs = {}
+
+        if FupComponents.WEB in component_list:
+            outputs['WEB'] = (
+                components.WebComponent(
+                    stack_name=stack_name,
+                    directory=web_path
+                ).status()
+            )
+
+        if FupComponents.API in component_list:
+            outputs['API'] = (
+                components.APIComponent(
+                    stack_name=stack_name,
+                    directory=api_path
+                ).status()
+            )
+        return outputs
+
+    def teardown(self, stack_name: str, component_list) -> bool:
+        self._log(stylize(
+            f"Tearing down [{stack_name}]...",
+            colored.fg("red"), colored.attr('bold')
+        ))
+        stack = self.stack_db.get(stack_name)
+
+        if FupComponents.DB in component_list:
+            if stack.db_deployed != "0":
+                # TODO: Tear down deployed assets
+                pass
+
+        if FupComponents.API in component_list:
+            if stack.api_deployed != "0":
+                try:
+                    components.APIComponent(
+                        stack_name=stack.api_deployed
+                    ).teardown()
+                    stack.api_deployed = "0"
+                    stack.save()
+                except Exception as e:
+                    raise ValueError("Could not remove API component: {}".format(e))
+
+        if FupComponents.WEB in component_list:
+            if stack.web_deployed != "0":
+                try:
+                    components.WebComponent(
+                        stack_name=stack.web_deployed
+                    ).teardown()
+                    stack.web_deployed = "0"
+                    stack.save()
+                except Exception as e:
+                    raise ValueError("Could not remove WEB component: {}".format(e))
+        self._success(f"Successfully tore down stack [{stack_name}.{component_list}].")
+        stack = self.stack_db.get(stack_name)
+        if stack.web_deployed == stack.api_deployed == stack.db_deployed == "0":
+            self._success(f"Completely removing all traces of stack [{stack_name}].")
+            return stack.delete()
+            self._success("Done!")
+        else:
+            return False
+
+    def get_stacks(self) -> List[str]:
+        try:
+            return [
+                i.stack_name for i in self.stack_db.scan()
+            ]
+        except:
+            return []
+
--- fup/components.py
+++ fup/components.py
@@ -1,13 +1,17 @@
 import abc
 from abc import abstractmethod
 
+import json
+import mimetypes
+import os
+import subprocess
 from uuid import uuid4
 
+import yaml
+
+import boto3
 from pynamodb.attributes import BooleanAttribute, UnicodeAttribute, MapAttribute, UTCDateTimeAttribute, NumberAttribute
 from pynamodb.models import Model
-
-
-import yaml
 
 
 class Component(abc.ABC):
@@ -32,86 +36,192 @@ class Component(abc.ABC):
 class WebComponent(Component):
 
     def __init__(self, **kwargs):
-        pass
+        self.client = boto3.client('s3')
+        self.bucket_name = kwargs['stack_name']
+        self.directory = kwargs.get('directory')
 
+        self.website_configuration = {
+            'ErrorDocument': {'Key': 'index.html'},
+            'IndexDocument': {'Suffix': 'index.html'},
+        }
 
-_attribute_lookup = {
-    'string': 'UnicodeAttribute',
-    'date': 'UTCDateTimeAttribute',
-    'object': 'MapAttribute',
-    'number': 'NumberAttribute'
-}
-
-_table_class_fmt_base = """
-class {}(Model):
-
-    class Meta:
-        table_name = "{}"
-
-{}
-"""
-
-def _rewrite_schema(schema):
-    _schema = {}
-    for col, type in schema.items():
-        hash_key = False
-        if type.endswith("*"):
-            type = type.rstrip("*")
-            hash_key = True
-        _schema[col] = _attribute_lookup[type] + (
-            "(hash_key=True)" if hash_key else "()"
+    def init(self, **kwargs):
+        # create the bucket
+        self.bucket = self.client.create_bucket(
+            Bucket=self.bucket_name,
+            ACL='public-read'
         )
-    return _schema
+        self.client.put_bucket_website(
+            Bucket=self.bucket_name,
+            WebsiteConfiguration=self.website_configuration
+        )
+        self.update()
+        return self.bucket_name
 
-def _inject_pynamo_class(table_name: str, schema: dict):
-    """
-    Extremely polluting call to create a class in the global namespace
-    using exec. Obviously dangerous. Don't be dumb.
+    def update(self, **kwargs):
+        session = boto3.Session()
+        s3 = session.resource("s3")
+        for root, dirs, files in os.walk(self.directory):
+            for name in files:
+                path = root.split(os.path.sep)
+                path.append(name)
+                mimetype = mimetypes.guess_type(os.path.sep.join(path))[0]
+                if mimetype is None:
+                    print("Skipping file {} with no MIME type.".format(name))
+                    continue
+                print(mimetype + "\t" + os.path.sep.join(path))
+                s3.Bucket(self.bucket_name).upload_file(
+                    os.path.sep.join(path),
+                    os.path.sep.join(path[2:]),
+                    ExtraArgs={
+                        "ContentType": mimetype,
+                        'GrantRead': 'uri="http://acs.amazonaws.com/groups/global/AllUsers"',
+                    }
+                )
+        return self.bucket_name
 
-    To avoid collisions, suffixes with uuid4.
 
-    Returns a pointer to that class.
-    """
-    unique_tablename = table_name + str(uuid4()).replace("-", "")
-    stringified_schema = ""
-    for col, type in schema.items():
-        stringified_schema += "\t{} = {}".format(col, type)
-    print(_table_class_fmt_base.format(
-        unique_tablename,
-        table_name,
-        stringified_schema
-    ))
-    return unique_tablename
+    def status(self, **kwargs):
+        return {
+            "url": f"https://{self.bucket_name}.s3.amazonaws.com/"
+        }
+
+    def teardown(self, **kwargs):
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(self.bucket_name)
+        bucket.objects.all().delete()
+        return bucket.delete()
 
 class DBComponent(Component):
 
     def __init__(self, **kwargs):
-        self.schema = kwargs.get('schema', None)
-        schemafile = kwargs.get('schemafile', None)
-        if self.schema is None and schemafile is None:
-            raise ValueError("Must specify one of schema or schemafile.")
-        if self.schema is None:
-            self.schema = yaml.load(open(schemafile))
-
-    def init(self, **kwargs):
-        for table, schema in self.schema.items():
-            print(table)
-            print(_rewrite_schema(schema))
-            print(_inject_pynamo_class(table, _rewrite_schema(schema)))
-        raise NotImplementedError()
-
-    def update(self, **kwargs):
-        raise NotImplementedError()
-
-    def status(self, **kwargs):
-        raise NotImplementedError()
-
-    def teardown(self, **kwargs):
-        raise NotImplementedError()
-
+        pass
 
 
 class APIComponent(Component):
 
     def __init__(self, **kwargs):
-        pass
+        self.stack_name = kwargs['stack_name']
+        self.directory = kwargs['directory']
+        _default_config = {
+            self.stack_name: {
+                "s3_bucket": self.stack_name + "-api",
+                "app_function": "main.APP",
+                "aws_region": "us-east-1",
+            }
+        }
+        pwd = os.getcwd()
+        os.chdir(self.directory)
+        self.config = kwargs.get('config', _default_config)
+        if self.stack_name not in self.config:
+            self.config[self.stack_name] = _default_config[self.stack_name]
+        with open("./zappa_settings.json", 'w') as fh:
+            fh.write(json.dumps(self.config))
+        os.chdir(pwd)
+
+    def __del__(self):
+        """
+        Remove the zappa_settings.json tempfile.
+        Arguments:
+            None
+        """
+        pwd = os.getcwd()
+        try:
+            os.chdir(self.directory)
+            os.remove("./zappa_settings.json")
+        except Exception:
+            pass
+        os.chdir(pwd)
+
+    def init(self, **kwargs):
+        # create zappa file
+        pwd = os.getcwd()
+        os.chdir(self.directory)
+        print(
+            subprocess.run(
+                "pip3 install -U flask zappa",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout
+        )
+        print(
+            subprocess.run(
+                "pip3 install -r ./requirements.txt",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout
+        )
+        # zappa deploy (update)
+        print(
+            subprocess.run(
+                f"zappa deploy {self.stack_name}",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout
+        )
+        os.chdir(pwd)
+        return json.dumps(self.config)
+        # return zappa config json
+
+    def update(self, **kwargs):
+        pwd = os.getcwd()
+        os.chdir(self.directory)
+        print(
+            subprocess.run(
+                "pip3 install -U flask zappa",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout
+        )
+        print(
+            subprocess.run(
+                "pip3 install -r ./requirements.txt",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout
+        )
+        print(
+            subprocess.run(
+                f"zappa update {self.stack_name}",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout
+        )
+        os.chdir(pwd)
+        return json.dumps(self.config)
+
+
+    def status(self, **kwargs):
+        pwd = os.getcwd()
+        os.chdir(self.directory)
+        result = (
+            subprocess.run(
+                f"zappa status {self.stack_name}",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout
+        ).decode('utf-8')
+        os.chdir(pwd)
+        results = {
+            line.split(":")[0].strip(): ":".join(line.split(":")[1:]).strip()
+            for line in result.split("\n")
+            if (
+                ('Invocations' in line) or
+                ('Errors' in line) or
+                ('Error Rate' in line) or
+                ('API Gateway URL' in line)
+            )
+        }
+        return results
+
+    def teardown(self, **kwargs):
+        pwd = os.getcwd()
+        os.chdir(self.directory)
+        print(
+            subprocess.run(
+                f"zappa undeploy {self.stack_name}",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout
+        )
+        os.chdir(pwd)
